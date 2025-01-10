ad7d07331fec23bfbde7fa4f473e690f2144d7f3
ielhag19682024 <ielhag@gmail.com>
2025-01-10 14:50:03 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "demo-resources"
-    storage_account_name = "techtutorialswithpiyush"
-    container_name       = "prod-tfstate"
+    storage_account_name = "techibrahimdemo"
+    container_name       = "prod-tfstate2"
     key                  = "prod.terraform.tfstate"
   }
 }
